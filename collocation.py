56452c9aee784b819313962f92175fcfc86d3fad
Nickolai Belakovski <nbelakovski@gmail.com>
2025-06-17 15:08:41 -0400

    A few error have been corrected, and I now have it working for generic t0-tf
    It's struggling mightily on a larger domain, and so I think adding
    support for "finite elements" is necessary in order to make this work on
    larger domains. I could also probably use a better representation, but
    I'm curious as to how far this naive one can go and this exercise is
    interesting for showing the various pitfalls that this approach can fall
    into.

--- collocation.py
+++ collocation.py
@@ -20,7 +20,7 @@
 import numpy as np
 import jax.numpy as jnp
 from scipy.special import roots_legendre
-from scipy.optimize import fsolve
+from scipy.optimize import fsolve, root
 from jax import grad, jacfwd, jacrev
 
 def representation(t, params):
@@ -31,10 +31,12 @@ reprgrad = grad(representation)
 
 # Hm, this seems hardcoded to a single variable, what if x is a vector?
 def solve(dxdt, x0, t0, tf, K, N=1):  # dxdt - > function of t, x
-    roots, weights = roots_legendre(K)  # Gives K roots
-    roots += 1
-    roots /= 2
-    roots = np.linspace(t0+(tf-t0)/K, tf, K)
+    # TODO: I'd like to try splitting up the domain with multiple elements, i.e. multiple
+    # representations, and of course that means more coefficients.
+    roots, weights = roots_legendre(K-1)
+    roots *= (tf - t0)/2  # scale
+    roots += (tf - t0)/2 + t0  # then shift, to 0 and then to t0
+    # roots = np.linspace(t0+(tf-t0)/K, tf, K-1)
     print(roots)
     # What I really need is to convert whatever representation I have into a matrix form
     # So that I can have Ax=b where x = [a0, a1, a2, ...] and b = dxdt(ti), and A is [1, t, t**2, ...; 1, t, t**2, ...; ...] where each row of A corresponds to a root from roots_legendre, except the first row which is the IC
@@ -51,13 +53,37 @@ def solve(dxdt, x0, t0, tf, K, N=1):  # dxdt - > function of t, x
     # logic, or at least it would if we wanted to reduce things, but I guess we could jsut use a nonlinear
     # solver and treat the right side as a black box that's just a function of t, i.e. our x is just coming
     # from our representation.
-    def functosolve(x, t):
-        eq1 = [representation(t[0], params=x) - x0]
-        return eq1 + [reprgrad(ti, params=x) - dxdt(t, representation(ti, params=x)) for ti in t[1:]]
-    result, infodict, ier, mesg = fsolve(functosolve, x0=np.ones(K), fprime=jacrev(functosolve), args=roots, full_output=True)  # np.linspace(t0, tf, K)
-    print(mesg, functosolve(result, np.hstack((0, roots))))
-    assert ier == 1, "ier !=1, the solution did not converge"
+    def create_system_of_eqns(t0, x0, roots, representation, reprgrad):
+        def system(params):
+            eq1 = [representation(t0, params) - x0]
+            eq2_to_n = [reprgrad(ti, params) - dxdt(ti, representation(ti, params)) for ti in roots]
+            return eq1 + eq2_to_n
+        return system
+
+    # # result, infodict, ier, mesg = fsolve(functosolve, x0=np.zeros(K), fprime=jacrev(functosolve), args=roots, full_output=True, xtol=1e-12)  # np.linspace(t0, tf, K)
+
+    # # x1 = x0 - f'(x0)^-1 @ f(x0)
+    # # f'(x0)dx = -f(x0)
+    # x = np.zeros(K)
+    # jacobian = jacrev(functosolve)
+    # for j in range(maxiter:=50):
+    #     np.linalg.solve(jacobian(x, roots), )
+
+
+    # return None  # Below here is the old version using scipy's root
+    # def functosolve(x, t):
+    #     eq1 = [representation(t0, params=x) - x0]
+    #     return eq1 + [reprgrad(ti, params=x) - dxdt(ti, representation(ti, params=x)) for ti in t]
+    
+    functosolve = create_system_of_eqns(t0, x0, roots, representation, reprgrad)
+
+    sol = root(functosolve, x0=np.ones(K), jac=jacrev(functosolve), method='hybr', tol=1e-3)  # np.linspace(t0, tf, K)
+
+    # I think that since I have the jacobian I might as well just implement Newton's method, it seems
+    # really strange to me that fsolve/root are returning value that are nowhere near a root.
+    print(sol.message, functosolve(sol.x))
+    assert sol.success, "sol.success is false, the solution did not converge"
 
     # So, trying collocation with monomial basis of degree 4 and uniform grid got me a total error of 0.177. Degree 5 and above did not converge
     # Collocation with monomial basis and legendre roots only converged at K=2, i.e. linear
-    return result
+    return sol.x
--- example.py
+++ example.py
@@ -14,6 +14,8 @@
 # To sync the local venv with these requirements, run:
 #!VIRTUAL_ENV=.venv uv sync --script example.py --active
 #!%reset -f
+#!%load_ext autoreload
+#!%autoreload now
 
 
 import numpy as np
@@ -27,9 +29,19 @@ def dzdt(t, z):
 def analytical(t):
     return (4*t - 3)/(4*t + 1)
 
-coefficients = solve(dzdt, -3, 0, 1, 5)
+t0 = -0.1
+tf = 4
+coefficients = solve(dzdt, analytical(t0), t0, tf, 9)
+# Uniform grid failed to converge on 5, 6*, 8*, 9, 10
+# Legendre roots failed to converge on 7, 10
+# * It converged, but to an incorrect local minimum
+# On 7, uniform grid had total error of 0.019473126645063787
+# On 6, Legendre had error of 0.009999763241381451, ~50% better than uniform
+# On 8 it got 0.001374732661305742 but there was an integration warning
+# So choice of grid has a significant impact on convergence as well as on error
+print(f'{coefficients=}')
 
-plt.plot(t:=np.linspace(0, 1), analytical(t), color='b')
+plt.plot(t:=np.linspace(t0, tf), analytical(t), color='b')
 plt.plot(t, [representation(ti, coefficients) for ti in t], color='r')
 plt.grid()
 
