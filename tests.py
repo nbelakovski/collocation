1f81c67064623bdc3dea2b47369f1c13a4d3bd74
Nickolai Belakovski <nbelakovski@gmail.com>
2025-06-24 14:50:09 -0400

    Add single element test

--- tests.py
+++ tests.py
@@ -13,3 +13,16 @@ def test_multi_element_state():
     solution = solve(dzdt, x0, t0, tf, K=3, representation_str='monomial', N=1)
     assert np.allclose(solution(t0), x0)
     assert np.allclose(solution(tf), [np.cos(tf), -np.sin(tf)], rtol=1e-4, atol=1e-4)
+
+
+def test_single_element_state():
+    def dzdt(t, z):
+        return [z[0]]
+
+    t0 = 0
+    tf = 1
+    x0 = np.array([1])
+    # With this x0 the analytical solution is x(t) = cos(t)
+    solution = solve(dzdt, x0, t0, tf, K=3, representation_str='monomial', N=1)
+    assert np.allclose(solution(t0), x0)
+    assert np.allclose(solution(tf), [np.exp(tf)], rtol=1e-4, atol=1e-4)
